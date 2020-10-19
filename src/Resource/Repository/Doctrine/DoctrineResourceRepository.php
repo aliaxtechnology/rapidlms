c20b31226e1bcb42f16c2846aac4c5abc172368b
monir ait <aliaxtechnology@gmail.com>
2020-10-19 20:23:14 +0200

    doctrine resource repository

---
+++ src/Resource/Repository/Doctrine/DoctrineResourceRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Lms\Resource\Repository\Doctrine;
+
+
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Lms\Resource\Entity\Resource;
+use Lms\Resource\Entity\ResourceId;
+use Lms\Resource\Repository\ResourceRepository;
+
+class DoctrineResourceRepository implements ResourceRepository
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
+    public function nextIdentity(): ResourceId
+    {
+        $id = $this->em->getConnection()->transactional(function (Connection $connection) {
+
+            $connection->insert('resource_sequence', []);
+
+            return $connection->lastInsertId();
+        });
+
+        return new ResourceId($id);
+    }
+    
+
+    public function save(Resource $resource): void
+    {
+        $this->em->persist($resource);
+        $this->em->flush();
+    }
+}
