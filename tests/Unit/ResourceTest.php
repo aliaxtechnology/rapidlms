e21d0b475a8d8fc2b2c93784ecad7d978027bdeb
monir ait <aliaxtechnology@gmail.com>
2020-10-20 21:20:20 +0200

    update unit test resource created

--- tests/Unit/ResourceTest.php
+++ tests/Unit/ResourceTest.php
@@ -4,8 +4,10 @@
 namespace Lms\Tests\Unit;
 
 
+use Lms\Resource\Entity\PreviewId;
 use Lms\Resource\Entity\Resource;
 use Lms\Resource\Entity\ResourceId;
+use Lms\Resource\Entity\ResourceType;
 use Lms\Resource\Event\ResourceCreated;
 use PHPUnit\Framework\TestCase;
 
@@ -17,16 +19,26 @@ class ResourceTest extends TestCase
         // GIVEN
         $resourceId = new ResourceId(1);
         $name = 'Module 1';
+        $type = ResourceType::fromString('quiz');
+        $previewId = new PreviewId(5);
+
 
         // WHEN
-        $resource = Resource::create($resourceId, $name);
+        $resource = Resource::create($resourceId, $name, $type, $previewId);
 
         // THEN
         $events = $resource->popEvents();
 
         $this->assertCount(1, $events);
         $this->assertInstanceOf(ResourceCreated::class, $events[0]);
-        $this->assertSame(['resource_id' => $resourceId->asString(), 'name' => $name], $events[0]->asArray());
+
+        $expected = [
+            'resource_id' => $resourceId->asString(),
+            'name' => $name,
+            'type' => $type->asString(),
+            'preview_id' => $previewId->asString(),
+        ];
+        $this->assertSame($expected, $events[0]->asArray());
 
     }
 }
